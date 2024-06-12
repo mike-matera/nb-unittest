777a70b57b1d2551b7dfd7ccfeba50816b57a533
Mike Matera <michael.matera@cabrillo.edu>
2024-06-11 23:26:30 -0700

    Allow composable test flow, update examples.

---
+++ examples/test_lib.py
@@ -0,0 +1,16 @@
+"""
+Importable test cases for nbtest
+"""
+
+import unittest
+
+from nbtest.magic import nbtest_attrs
+
+
+class ImportableClass(unittest.TestCase):
+    """An importable test case."""
+
+    def test_reusable(self):
+        """This is a test case in a reusable test."""
+        for attr in nbtest_attrs:
+            assert False, f"Just testing for now: {attr}"
--- nbtest/magic.py
+++ nbtest/magic.py
@@ -13,6 +13,9 @@ from IPython.display import HTML
 from .templ import templ
 from .unit import NotebookTestRunner
 
+nbtest_attrs = []
+runner_class = NotebookTestRunner
+
 
 @magics_class
 class TestCache(Magics):
@@ -31,14 +34,20 @@ class TestCache(Magics):
         """
         A cell magic that finds and runs unit tests on selected symbols.
         """
+        global nbtest_attrs
+
+        self._test_ns["nbtest_cases"] = None
+        nbtest_attrs = []
 
         # Find the symbols mentioned in the cell magic
         try:
             for symbol in (x.strip() for x in line.split()):
                 if symbol.startswith("@"):
                     self._test_ns[symbol[1:]] = self._cache[symbol]
+                    nbtest_attrs.append(symbol[1:])
                 else:
                     self._test_ns[symbol] = self.shell.user_ns[symbol]
+                    nbtest_attrs.append(symbol)
         except KeyError as e:
             return HTML(templ.missing.render(error=e))
 
@@ -52,26 +61,52 @@ class TestCache(Magics):
         # Find and execute test cases.
         suite = unittest.TestSuite()
 
-        class top_test_visitor(ast.NodeVisitor):
-            """Parse the input cell to find top level test class and test function defs"""
-
-            def visit_ClassDef(_, node):
-                if isinstance(self._test_ns[node.name], type) and issubclass(
-                    self._test_ns[node.name], unittest.TestCase
-                ):
-                    suite.addTest(
-                        unittest.defaultTestLoader.loadTestsFromTestCase(self._test_ns[node.name])
+        if self._test_ns["nbtest_cases"] is not None:
+            # Test cases are specified
+            for case in self._test_ns["nbtest_cases"]:
+                if isinstance(case, type) and issubclass(case, unittest.TestCase):
+                    suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(case))
+                elif callable(case):
+                    for gen_case in case():
+                        if isinstance(gen_case, type) and issubclass(gen_case, unittest.TestCase):
+                            suite.addTest(
+                                unittest.defaultTestLoader.loadTestsFromTestCase(gen_case)
+                            )
+                        elif isinstance(gen_case, unittest.TestCase):
+                            suite.addTest(gen_case)
+                elif isinstance(
+                    case, re.__class__
+                ):  # Couldn't figure out how to reference module class
+                    suite.addTest(unittest.defaultTestLoader.loadTestsFromModule(case))
+                else:
+                    raise ValueError(
+                        f"Invalid value in test_cases: {case}. Entries must be a unittest.TestCase, a function that returns a list of TestCases or a module."
                     )
-                # do not descend
 
-            def visit_FunctionDef(_, node):
-                if node.name.startswith("test") and callable(self._test_ns[node.name]):
-                    suite.addTest(unittest.FunctionTestCase(self._test_ns[node.name]))
-                # do not descend
+        else:
+            # Look for cases in the cell
+            class top_test_visitor(ast.NodeVisitor):
+                """Parse the input cell to find top level test class and test function defs"""
 
-        top_test_visitor().visit(tree)
+                def visit_ClassDef(_, node):
+                    if isinstance(self._test_ns[node.name], type) and issubclass(
+                        self._test_ns[node.name], unittest.TestCase
+                    ):
+                        suite.addTest(
+                            unittest.defaultTestLoader.loadTestsFromTestCase(
+                                self._test_ns[node.name]
+                            )
+                        )
+                    # do not descend
 
-        runner = NotebookTestRunner()
+                def visit_FunctionDef(_, node):
+                    if node.name.startswith("test") and callable(self._test_ns[node.name]):
+                        suite.addTest(unittest.FunctionTestCase(self._test_ns[node.name]))
+                    # do not descend
+
+            top_test_visitor().visit(tree)
+
+        runner = runner_class()
         result = runner.run(suite)
         if result.wasSuccessful():
             return HTML(templ.ok.render(result=result))
