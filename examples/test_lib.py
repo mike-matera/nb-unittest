f1db3b0cf4a0913ccd23b088f80486aeac4f1aa0
Mike Matera <michael.matera@cabrillo.edu>
2024-06-21 11:54:38 -0700

    Update documentation of composable tests.

--- examples/test_lib.py
+++ examples/test_lib.py
@@ -4,13 +4,49 @@ Importable test cases for nbtest
 
 import unittest
 
-from nbtest.cache import nbtest_attrs
+from nbtest import nbtest_attrs
 
 
-class ImportableClass(unittest.TestCase):
+class ATestCase(unittest.TestCase):
     """An importable test case."""
 
     def test_reusable(self):
-        """This is a test case in a reusable test."""
-        for attr in nbtest_attrs:
-            assert False, f"Just testing for now: {attr}"
+        self.fail()
+
+
+class CustomizableTestCase(unittest.TestCase):
+    """A test case that is made to be customized."""
+
+    fail_message = "Default failure message."
+
+    def test_customized(self):
+        """Just an example of customization."""
+        self.fail(self.fail_message)
+
+
+class DerivedCase1(CustomizableTestCase):
+    fail_message = "Failure message customized by inheritance"
+
+
+def deco(**kwargs):
+    def _deco(cls):
+        return type(cls.__name__, (cls,), kwargs)
+
+    return _deco
+
+
+@deco(fail_message="Failure message customized by a decorator.")
+class DerivedCase2(CustomizableTestCase):
+    pass
+
+
+def lib_function():
+    """A function that returns a test or tests."""
+    suite = unittest.TestSuite()
+    for attr in nbtest_attrs:
+
+        class _tc(CustomizableTestCase):
+            fail_message = f"Customized for attribute {attr}"
+
+        suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(_tc))
+    return suite
